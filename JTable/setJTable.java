0923e0bd91878a47667f51a25f4db9ee1964b3bc
izal <58200011+rzlahd@users.noreply.github.com>
2023-05-30 15:28:29 -0800

    Create setJTable.java

---
+++ JTable/setJTable.java
@@ -0,0 +1,22 @@
+DefaultTableModel tabModel;
+
+private void setJTable() {
+    // Atur Data Table
+    String[] Kolom = {"No", "Kode Book", "Title"};
+    tabModel = new DefaultTableModel(null, Kolom) {
+        boolean[] canEdit = new boolean[]{false, false, false};
+
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            return canEdit[columnIndex];
+        }
+    };
+    // Atur ukuran JTable
+    TBuku.setModel(tabModel);
+    TBuku.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+    TBuku.getColumnModel().getColumn(0).setPreferredWidth(30);
+    TBuku.getColumnModel().getColumn(1).setPreferredWidth(100);
+    TBuku.getColumnModel().getColumn(2).setPreferredWidth(200);
+
+    getData();
+}
